58fe20217c63feb756887d61a4def335ef8e7c32
ismail264 <ismailaz984@gmail.com>
2025-08-02 23:44:52 +0300

    Add files via upload

---
+++ Yeni klasör (3)/websayfası.js
@@ -0,0 +1,7 @@
+// Sayfa yüklendiğinde konsola mesaj yaz
+console.log("Hoş geldin! İsmail'in kişisel web sitesi aktif.");
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Kullanıcıya alert göster
+  alert("Web siteme hoş geldin!");
+});
