ebb444fb9e0dbba3865e18fe19539e1811e04ed6
ismail264 <ismailaz984@gmail.com>
2025-08-03 22:56:43 +0300

    Delete Yeni klasör (3) directory

--- Yeni klasör (3)/websayfası.js
+++
@@ -1,7 +0,0 @@
-// Sayfa yüklendiğinde konsola mesaj yaz
-console.log("Hoş geldin! İsmail'in kişisel web sitesi aktif.");
-
-document.addEventListener("DOMContentLoaded", function() {
-  // Kullanıcıya alert göster
-  alert("Web siteme hoş geldin!");
-});
