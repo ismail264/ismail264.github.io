0fe72bae48017e523dcac57d841a5c8a6c919d5e
Ismail Aktas <ismailaz984@gmail.com>
2025-08-04 09:48:03 +0300

    Dosyalar kök dizine taşındı

---
+++ index.js
@@ -0,0 +1,7 @@
+// Sayfa yüklendiğinde konsola mesaj yaz
+console.log("olduu");
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Kullanıcıya alert göster
+  alert("web siteme hoş geldin");
+});
